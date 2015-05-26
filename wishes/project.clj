32d7414a7116cef1081c23f8532bd57066d2de3b
Ilya Shinkarenko <shinych@gmail.com>
2015-05-26 21:25:05 +0200

    basic http server with a single route and 404 page

--- wishes/project.clj
+++ wishes/project.clj
@@ -1,4 +1,13 @@
 (defproject wishes "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+    [http-kit "2.1.18"]
+    [compojure "1.1.6"]
+    [org.clojure/data.json "0.2.4"]
+    
+
+  ]
+
   :source-paths ["src/clj"]
+  
   :main wishes.core)
--- wishes/src/clj/wishes/core.clj
+++ wishes/src/clj/wishes/core.clj
@@ -1,5 +1,47 @@
-(ns wishes.core)
+(ns wishes.core
+  (:use 
+     [org.httpkit.server :as http-kit :only [run-server]])
+  (:require
+     [clojure.data.json :as json]
+
+     [compojure.core :as c.core]
+     [compojure.route :as c.route]
+
+     [ring.util.response :as r.util]
+
+     ))
 
 
-(defn -main [port] 
-  (println (str "This will start the server on port " port)))
+;; a function stopping the HTTP server
+(defonce stop-server (atom nil))
+
+(defonce wishes [
+  {:wish "Live long" :for "Everybody" :motivation :positive}
+  {:wish "Go home" :for "Me" :motivation :neutral}
+  {:wish "Suffer" :for "My enemies" :motivation :negative}
+  {:wish "Prosper" :for "My family" :motivation :positive}])
+
+(defn get-wishes []
+  (->
+    (json/write-str wishes) ;;XXX why JSON? ;-) use EDN as is
+    (r.util/response) 
+    (r.util/content-type "application/json")))
+
+(c.core/defroutes app-routes
+  (c.core/GET  "/wishes" [] (get-wishes))
+  (c.route/resources "/")
+  (c.route/not-found (slurp "www/404.html")))
+
+(defn start "Starts the HTTP server on the given port" [port]
+  (println (format "Starting HTTP server on port #%s..." port))
+  ;; run-server returns a function that stops the server
+  (reset! stop-server (http-kit/run-server #'app-routes {:port port})))
+
+(defn stop "Stops the HTTP server" []
+  (when @stop-server
+    (@stop-server)
+    (reset! stop-server nil)      
+    (println "HTTP server stopped")))
+
+(defn -main [port-str] 
+  (start (Integer/valueOf port-str)))
