bb0ba16f084596c7ca007cc8b12aaa61d04406d5
Ilya Shinkarenko <shinych@gmail.com>
2015-05-26 22:02:18 +0200

    rendering HTML from a hardcoded data

--- wishes/src/cljs/wishes/ui.cljs
+++ wishes/src/cljs/wishes/ui.cljs
@@ -4,10 +4,21 @@
 
 (enable-console-print!)
 
-(defn c-hello-world []
-  [:h1 "Hello, World!"])
+(defn get-wishes [] [
+  {:wish "Live long" :for "Everybody" :motivation :positive}
+  {:wish "Go home" :for "Me" :motivation :neutral}
+  {:wish "Suffer" :for "My enemies" :motivation :negative}
+  {:wish "Prosper" :for "My family" :motivation :positive}])
+
+(defn c-wish [{:keys [motivation wish for]}]
+  [:div (str "I wish " for " to " wish " with " (name motivation) " motivation")]  )
+
+(defn c-body []
+  [:div
+    [:h1 "Wishes"]
+    (map c-wish (get-wishes))])
 
 (defn ^:export run [] 
   (println "Started web-application")
-  (reagent/render-component [c-hello-world]
+  (reagent/render-component [c-body]
     (.-body js/document)))
