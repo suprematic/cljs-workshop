257b04d9a85396cba8a9c6896f599f9a9bbbbc4f
Ilya Shinkarenko <shinych@gmail.com>
2015-05-26 21:56:16 +0200

    set up cljs/react/reagent skeleton

--- wishes/project.clj
+++ wishes/project.clj
@@ -1,13 +1,27 @@
 (defproject wishes "0.1.0-SNAPSHOT"
+  
   :dependencies [
     [org.clojure/clojure "1.6.0"]
     [http-kit "2.1.18"]
     [compojure "1.1.6"]
     [org.clojure/data.json "0.2.4"]
-    
 
+    [org.clojure/clojurescript "0.0-2156"]
+    [reagent "0.3.0"]
+  ]
+
+  :plugins[
+     [lein-cljsbuild "1.0.2"]
   ]
 
   :source-paths ["src/clj"]
   
-  :main wishes.core)
+  :main wishes.core
+
+  :cljsbuild {:builds  [{:id "dev"
+                         :source-paths ["src/cljs"]
+                         :compiler {:output-dir "www/js/compiled"
+                                    :output-to  "www/js/compiled/wishes.js"
+                                    :source-map "www/js/compiled/wishes.js.map"
+                                    :optimizations :none
+                                    :pretty-print true}}]})
--- wishes/src/cljs/wishes/ui.cljs
+++ wishes/src/cljs/wishes/ui.cljs
@@ -1,3 +1,13 @@
-(ns wishes.ui)
+(ns wishes.ui
+  (:require 
+    [reagent.core :as reagent :refer [atom]]))
 
-;; there will be a UI code later
+(enable-console-print!)
+
+(defn c-hello-world []
+  [:h1 "Hello, World!"])
+
+(defn ^:export run [] 
+  (println "Started web-application")
+  (reagent/render-component [c-hello-world]
+    (.-body js/document)))
