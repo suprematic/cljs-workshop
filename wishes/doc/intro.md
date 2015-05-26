78b9324128d6ee22a4b45b4bed70e06737c7d0b2
Ilya Shinkarenko <shinych@gmail.com>
2015-05-26 20:49:20 +0200

    lein new wishes

---
+++ wishes/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wishes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ wishes/test/wishes/core_test.clj
@@ -0,0 +1,7 @@
+(ns wishes.core-test
+  (:require [clojure.test :refer :all]
+            [wishes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
