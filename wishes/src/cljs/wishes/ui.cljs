20247184b4cd17eb19fd2a5bb3445500207df6a6
Ilya Shinkarenko <shinych@gmail.com>
2015-05-26 22:13:49 +0200

    get wishes from server

--- wishes/src/cljs/wishes/ui.cljs
+++ wishes/src/cljs/wishes/ui.cljs
@@ -1,24 +1,40 @@
 (ns wishes.ui
   (:require 
+    [clojure.walk :as cw]
     [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(defn get-wishes [] [
-  {:wish "Live long" :for "Everybody" :motivation :positive}
-  {:wish "Go home" :for "Me" :motivation :neutral}
-  {:wish "Suffer" :for "My enemies" :motivation :negative}
-  {:wish "Prosper" :for "My family" :motivation :positive}])
+(defonce serverURI "wishes")
+
+(def wishes-model (atom nil))
+
+(defn refresh-wishes []
+  (let [request (js/XMLHttpRequest.)]
+    (.open request "GET" serverURI false)
+    (set! (.-onload request) 
+      (fn []
+        (let [response (.-responseText request)
+              wishes (cw/keywordize-keys (js->clj (.parse js/JSON response)))
+              ]
+          (println (str "Got response: " response))
+          (println (str "Wishes      : " wishes))
+          (reset! wishes-model wishes))))
+    (.send request)))
 
 (defn c-wish [{:keys [motivation wish for]}]
   [:div (str "I wish " for " to " wish " with " (name motivation) " motivation")]  )
 
+(defn c-wishes []
+  (map c-wish @wishes-model))
+
 (defn c-body []
   [:div
     [:h1 "Wishes"]
-    (map c-wish (get-wishes))])
+    (c-wishes)])
 
 (defn ^:export run [] 
   (println "Started web-application")
   (reagent/render-component [c-body]
-    (.-body js/document)))
+    (.-body js/document))
+  (refresh-wishes))
